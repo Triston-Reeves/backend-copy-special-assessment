d0bd04c653c0c3b91eec780cc1c4c1efc3481189
Triston Reeves <triston.reeves123@gmail.com>
2020-07-20 09:43:07 -0400

    fixing copyspecial

--- copyspecial.py
+++ copyspecial.py
@@ -1,78 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Copyspecial Assignment"""
-
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-
 # give credits
-__author__ = "Triston"
-
+__author__ = "Ben McKenzie"
 import re
 import os
 import sys
 import shutil
 import subprocess
 import argparse
-
-
 def get_special_paths(dirname):
-    paths = os.listdir(dirname)
-    special_paths = []
-    for filename in paths:
-        match = re.search(r'__\w+__', filename)
-        if match:
-            special_paths.append(os.path.abspath(
-                os.path.join(dirname, filename)))
-
-    return special_paths
-
-
+    """Given a dirname, returns a list of all its special files."""
+    special_dir_files = []
+    for file in os.listdir(dirname):
+        special_file = re.search(r'__(\w+)__', file)
+        if special_file:
+            special_dir_files.append(os.path.abspath(os.path.join(dirname, file)))
+    #print(special_dir_files)
+    return special_dir_files
 def copy_to(path_list, dest_dir):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     for path in path_list:
-        print(dest_dir)
-        cmd = shutil.copy(path,dest_dir)
-    print("Command I'm going to do:")
-    print(' '.join(cmd))
-    for basename in path_list:
-        print(os.listdir(path_list))
-
+        fname = os.path.basename(path)
+        shutil.copy(path, os.path.join(dest_dir, fname))
 def zip_to(path_list, dest_zip):
-    cmd = ['zip', '-j', dest_zip]
-    cmd.extend(path_list)
-    print("Command I'm going to do:")
-    print(' '.join(cmd))
-    subprocess.check_output(cmd)
-
-
+    for path in path_list:
+        print(f'zip -j {dest_zip} {path}')
+        subprocess.run(['zip', '-j', dest_zip, path])
+    return
 def main(args):
     """Main driver code for copyspecial."""
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    parser.add_argument('from_dir', help='dest zipfile for special files')
+    # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='find dir for special files')
     ns = parser.parse_args(args)
     # TODO: you must write your own code to get the command line args.
     # Read the docs and examples for the argparse module about how to do this.
-
     # Parsing command line arguments is a must-have skill.
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
-
-    special_paths = get_special_paths(ns.from_dir)
-
+    # Your code here:p Invoke (call) your functions
+    if len(sys.argv) < 1:
+        parser.print_usage()
+    path_list = get_special_paths(ns.from_dir)
     if ns.todir:
-        copy_to(special_paths, ns.todir)
+        copy_to(path_list, ns.todir)
     elif ns.tozip:
-        zip_to(special_paths, ns.tozip)
-        print('\n'.join(special_paths))
-
-
+        zip_to(path_list, ns.tozip)
+    else:
+        print(*path_list, sep='\n')
 if __name__ == "__main__":
-
     main(sys.argv[1:])
